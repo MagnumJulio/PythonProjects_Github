dc25db976bab7814c471ca4322a608e668b4cac2
Magnum Julio <82421113+MagnumJulio@users.noreply.github.com>
2022-08-24 20:35:04 -0300

    Update auto-web.py

--- auto-web.py
+++ auto-web.py
@@ -44,15 +44,6 @@ def open_wpp(bomdia):
     #time.sleep(0.5)    
     #press('enter')
 
-
-def click_search_name(name):
-    x1, y1 = [207, 408]
-
-    moveTo(x1, y1)
-    #click()
-
-
-
 #espera 6:40 para executar programa
 '''
 while datetime.datetime.now().minute != 40 or datetime.datetime.now().hour != 5:
@@ -62,7 +53,7 @@ while datetime.datetime.now().minute != 40 or datetime.datetime.now().hour != 5:
 #login bom dia mercado
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-#options.add_argument("--headless")
+#options.add_experimental_option("--headless")
 driver = webdriver.Chrome(options=options)
 #https://www.bomdiamercado.com.br/exclusivo-assinante
 driver.get("https://www.bomdiamercado.com.br/exclusivo-assinante")
@@ -71,9 +62,9 @@ while len(driver.find_elements('xpath', '//*[@id="email"]')) < 1:
     time.sleep(2)
 
 time.sleep(5)
-driver.find_element('xpath', '//*[@id="email"]').send_keys('fernandolodi@capribr.com')
+driver.find_element('xpath', '//*[@id="email"]').send_keys('')
 time.sleep(5)
-driver.find_element('xpath', '//*[@id="password"]').send_keys('Caprifo@3220')
+driver.find_element('xpath', '//*[@id="password"]').send_keys('')
 time.sleep(5)
 driver.find_element('xpath', '//*[@id="next_button_default"]').click()
 time.sleep(5)
@@ -108,37 +99,8 @@ for texto in lista_soup:
 #bomdia = "Olá"
 pyperclip.copy(bomdia)
 print(bomdia)
-#deuruim
+
+driver.close()
+
 #enviar msg pelo wpp já aberto com pyautogui
-
-#lista de contatos
-contacts = ["Só Eu"]
-
-#manda mensagem para mim mesmo
-#bomdia = urllib.parse.quote(bomdia)
 open_wpp(bomdia)
-
-click_search_name("Só eu")
-
-'''
-#login whatsapp
-driver.get("https://web.whatsapp.com/")
-while len(driver.find_elements('id', 'side')) < 1:
-    time.sleep(1)
-
-#manda mensagem para mim mesmo
-bomdia = urllib.parse.quote(bomdia)
-link = f"https://web.whatsapp.com/send?phone={5531982544980}&text={bomdia}"
-driver.get(link)
-while len(driver.find_elements('id', 'side')) < 1:
-    time.sleep(1)
-
-#envia a msg
-while len(driver.find_elements('xpath', '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p/span')) < 1:
-    time.sleep(1)
-driver.find_element('xpath', '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p/span').send_keys(Keys.ENTER)
-
-time.sleep(10)
-'''
-
-
